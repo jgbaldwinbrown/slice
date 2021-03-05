495f68ba720031acfaae07652149e87b9500d143
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-03-05 16:54:31 -0700

    testing set

--- test_sub.c
+++ test_sub.c
@@ -8,5 +8,7 @@ int main() {
         print_slice(a, print_long_long);
     }
     print_slice(sub_slice(a, 5, 3), print_long_long);
+    set_slice_arr(sub_slice(a, 7, 2), (long long[]) {100, 1000}, 2);
+    print_slice(a, print_long_long);
     return 0;
 }
